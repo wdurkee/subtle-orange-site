8e6d3c040ab79f69997edff33830f63390107299
Will Durkee <william.w.durkee@gmail.com>
2021-02-05 11:54:28 -0800

    new stuff

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,27 @@
 import React from 'react'
+var ShopifyBuy = require('@shopify/buy-button-js')
 
 class Store extends React.Component {
     state = {  }
+
+    componentDidMount() {
+        var client = ShopifyBuy.buildClient({
+            domain: 'subtleorange.myshopify.com',
+            storefrontAccessToken: '24113dd2db4c23c1c4518123e7f40d40', // previously apiKey, now deprecated
+        });
+
+        var ui = ShopifyBuy.UI.init(client);
+
+        ui.createComponent('collection', {
+            id: 235486675101,
+            node: document.getElementById('my-collection')
+          });
+
+    }
+
     render() { 
         return ( 
-            <div>Link to Shopify</div>
+            <div id="my-collection">Store</div>
          );
     }
 }
