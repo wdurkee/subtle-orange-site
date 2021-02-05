b1c10f6aa8de614414dd25857bc94666d9421f3d
Will Durkee <william.w.durkee@gmail.com>
2021-02-05 12:09:17 -0800

    Updates

---
+++ precache-manifest.a3b5603481b0842afcdd43b3546e1807.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d9abebf598462001e9d7de681689b8",
+    "url": "/subtle-orange-site/index.html"
+  },
+  {
+    "revision": "eb51b160dcb3a115387f",
+    "url": "/subtle-orange-site/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f30dabf4b40e1e9d5c91",
+    "url": "/subtle-orange-site/static/css/main.5615c0bf.chunk.css"
+  },
+  {
+    "revision": "eb51b160dcb3a115387f",
+    "url": "/subtle-orange-site/static/js/2.314c963c.chunk.js"
+  },
+  {
+    "revision": "88f6a605040a28516ecc741c9d002240",
+    "url": "/subtle-orange-site/static/js/2.314c963c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30dabf4b40e1e9d5c91",
+    "url": "/subtle-orange-site/static/js/main.8b43577a.chunk.js"
+  },
+  {
+    "revision": "7b1fca9c7c0c3e3409e8",
+    "url": "/subtle-orange-site/static/js/runtime-main.ebc6f9ba.js"
+  },
+  {
+    "revision": "89d11a443c316da80dcb8f5e1f63c86e",
+    "url": "/subtle-orange-site/static/media/Bauhaus93.89d11a44.ttf"
+  },
+  {
+    "revision": "9ed24921966ec751782457643951cca0",
+    "url": "/subtle-orange-site/static/media/apple.9ed24921.png"
+  },
+  {
+    "revision": "45c0e656ba3e021c47940a3ebbfcceb9",
+    "url": "/subtle-orange-site/static/media/logo-slice.45c0e656.png"
+  },
+  {
+    "revision": "a2aeb3f7ee74c6d6a9e4575ead87f81a",
+    "url": "/subtle-orange-site/static/media/soundcloud.a2aeb3f7.svg"
+  },
+  {
+    "revision": "7c13d44746a52ee0d6445b90e0553a10",
+    "url": "/subtle-orange-site/static/media/spotify.7c13d447.png"
+  }
+]);
